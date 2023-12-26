125f1bfbfcb656cdfee0dc7eb81cf4b95dfc606b
Karol <114170012+karol199393@users.noreply.github.com>
2023-12-26 09:51:01 +0100

    Update Interface Validate

--- src/main/java/com/example/projectx/service/RegisterService.java
+++ src/main/java/com/example/projectx/service/RegisterService.java
@@ -1,10 +1,11 @@
 package com.example.projectx.service;
 
 import com.example.projectx.dto.User;
+import com.example.projectx.dto.Validate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RegisterService {
+public class RegisterService implements Validate {
 
     public User register(User user) {
         validateName(user.getName());
@@ -24,41 +25,41 @@ public class RegisterService {
         return user;
     }
 
-    private void validateName(String name) {
+    public void validateName(String name) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Field name can't be empty!");
         }
     }
 
-    private void validateSurname(String surname) {
+    public void validateSurname(String surname) {
         if (surname == null || surname.isEmpty()) {
             throw new IllegalArgumentException("Field surname can't be empty!");
         }
     }
 
-    private void validateEmail(String email) {
+    public void validateEmail(String email) {
         if(email == null || email.isEmpty() || !email.contains("@") || !email.matches(".+@.+\\..{2,}")) {
             throw new IllegalArgumentException("Invalid format email");
         }
     }
 
-    private void validatePassword(String password) {
+    public void validatePassword(String password) {
         if(password == null || password.isEmpty() ||password.length()<8)
             throw new IllegalArgumentException("Field password can't be empty and must be at least 8 character long");
     }
 
-    private void validatePhoneNumber(String phoneNumber) {
+    public void validatePhoneNumber(String phoneNumber) {
         if(phoneNumber == null || phoneNumber.isEmpty() || !phoneNumber.matches("[0-9]+"))
             throw new IllegalArgumentException("Field phone number can't be empty and must contain only digits");
     }
 
-    private void validatePesel(String pesel) {
+    public void validatePesel(String pesel) {
         if(pesel == null || pesel.isEmpty() || pesel.length() == 9)
             throw new IllegalArgumentException("Field pesel can't be empty and must be 9 digits!");
     }
 
-    private void validateAddress(String adress) {
-        if(adress == null || adress.isEmpty())
+    public void validateAddress(String address) {
+        if(address == null || address.isEmpty())
             throw new IllegalArgumentException("Field address can't be empty!");
 
     }
